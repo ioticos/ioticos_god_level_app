aa32047ebef572b92b5719e3cd5bcecc521b5d1d
Pablo <ioticos_mac@Pablos-iMac.local>
2020-12-11 15:36:42 -0300

    receive via $nuxt $on - we change the state of the icon when we receive the value

--- components/Widgets/Iotindicator.vue
+++ components/Widgets/Iotindicator.vue
@@ -1,11 +1,16 @@
 <template>
   <card>
     <div slot="header">
-      <h4 class="card-title">{{config.selectedDevice.name}} - {{config.variableFullName}} </h4>
+      <h4 class="card-title">
+        {{ config.selectedDevice.name }} - {{ config.variableFullName }}
+      </h4>
     </div>
 
-    <i class="fa " :class="[config.icon]"></i>
-
+    <i
+      class="fa "
+      :class="[config.icon, getIconColorClass()]"
+      style="font-size: 30px"
+    ></i>
   </card>
 </template>
 
@@ -14,6 +19,7 @@ export default {
   //props: ['config'],
   data() {
     return {
+      value: TextTrackCueList,
       config: {
         userId: "userid",
         selectedDevice: {
@@ -28,9 +34,40 @@ export default {
         icon: "fa-sun",
         column: "col-6",
         widget: "indicator",
-        class: "danger"
+        class: "success"
       }
     };
+  },
+  mounted(){
+    this.$nuxt.$on('widget-topic', this.processReceivedData)
+  },
+  methods: {
+
+    processReceivedData(data){
+        console.log("received");
+        console.log(data);
+        this.value = data.value;
+    },
+      
+    getIconColorClass() {
+      if (!this.value) {
+        return "text-dark";
+      }
+
+      if (this.config.class == "success") {
+        return "text-success";
+      }
+      if (this.config.class == "primary") {
+        return "text-primary";
+      }
+      if (this.config.class == "warning") {
+        return "text-warning";
+      }
+      if (this.config.class == "danger") {
+        return "text-danger";
+      }
+    }
+
   }
 };
 
--- pages/templates.vue
+++ pages/templates.vue
@@ -1,5 +1,27 @@
 <template>
     <div>
       <Iotindicator></Iotindicator>
+      <button @click="sendData()">SEND</button>
     </div>
 </template>
+
+<script>
+    export default {
+        data(){
+            return {
+                value:false
+            }
+        },
+
+        methods: {
+            sendData(){
+                this.value = !this.value;
+                const toSend = {
+                    value: this.value
+                }
+                this.$nuxt.$emit('widget-topic', toSend );
+
+            }
+        }
+    }
+</script>
