2c8a6c71dde10fda8f9ba2155dffe89b32815c74
Pablo <somosioticos@gmail.com>
2020-12-13 19:46:02 -0300

    mongo connection ready

--- api/index.js
+++ api/index.js
@@ -27,3 +27,55 @@ module.exports = app;
 app.listen(3001, () => {
   console.log("API server listening on port 3001");
 });
+
+//Mongo Connection
+const mongoUserName = "devuser";
+const mongoPassword = "devpassword";
+const mongoHost = "localhost";
+const mongoPort = "27017";
+const mongoDatabase = "ioticos_god_level";
+
+var uri =
+  "mongodb://" +
+  mongoUserName +
+  ":" +
+  mongoPassword +
+  "@" +
+  mongoHost +
+  ":" +
+  mongoPort +
+  "/" +
+  mongoDatabase;
+
+const options = {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+  authSource: "admin"
+};
+
+try {
+    mongoose.connect(uri, options).then(
+        () => {
+          console.log("\n");
+          console.log("*******************************".green);
+          console.log("✔ Mongo Successfully Connected!".green);
+          console.log("*******************************".green);
+          console.log("\n");
+        },
+        (err) => {
+          console.log("\n");
+          console.log("*******************************".red);
+          console.log("    Mongo Connection Failed    ".red);
+          console.log("*******************************".red);
+          console.log("\n");
+          console.log(err);
+        }
+      );
+} catch (error) {
+    console.log("ERROR CONNECTING MONGO ");
+    console.log(error);
+}
+
+
