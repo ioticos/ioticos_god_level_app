1545e03bb93fe8a81a3de8ac2a7c40584ef4c9e6
Pablo <somosioticos@gmail.com>
2020-12-15 17:29:11 -0300

    jwt and login ready

--- api/routes/users.js
+++ api/routes/users.js
@@ -10,46 +10,97 @@ import User from "../models/user.js";
 //GET -> req.query
 
 //AUTH
-router.post("/register", async (req, res) => {
-  try {
+router.post("/login", async (req, res) => {
 
-    const name = req.body.name;
+
     const email = req.body.email;
     const password = req.body.password;
-    const encryptedPassword = bcrypt.hashSync(password, 10);
 
-    const newUser = {
-      name: name,
-      email: email,
-      password: encryptedPassword
-    };
+    var user = await User.findOne({email: email});
 
-    var user = await User.create(newUser);
+    //if no email
+    if(!user){
+        const toSend = {
+            status: "error",
+            error: "Invalid Credentials"
+        }
+        return res.status(401).json(toSend);
+    }
 
-    console.log(user);
+    //if email and email ok
+    if (bcrypt.compareSync(password, user.password)){
 
-    const toSend = {
-      status: "success"
-    };
+        user.set('password', undefined, {strict: false});
 
-    res.status(200).json(toSend);
+        const token = jwt.sign({userData: user}, 'securePasswordHere', {expiresIn: 60 * 60 * 24 * 30});
 
-  } catch (error) {
-      
-      console.log("ERROR - REGISTER ENDPOINT")
-      console.log(error);
+        const toSend = {
+            status: "success",
+            token: token,
+            userData: user
+        }
 
-      const toSend = {
-        status: "error",
-        error: error
-      };
+        return res.json(toSend);
 
-      res.status(500).json(toSend);
+    }else{
+        const toSend = {
+            status: "error",
+            error: "Invalid Credentials"
+        }
+        return res.status(401).json(toSend);
+    }
 
-  }
+
+
 });
 
-router.post("/login", (req, res) => {});
+
+
+
+router.post("/register", async (req, res) => {
+    try {
+  
+      const name = req.body.name;
+      const email = req.body.email;
+      const password = req.body.password;
+      const encryptedPassword = bcrypt.hashSync(password, 10);
+  
+      const newUser = {
+        name: name,
+        email: email,
+        password: encryptedPassword
+      };
+  
+      var user = await User.create(newUser);
+  
+      console.log(user);
+  
+      const toSend = {
+        status: "success"
+      };
+  
+      res.status(200).json(toSend);
+  
+    } catch (error) {
+  
+        console.log("ERROR - REGISTER ENDPOINT")
+        console.log(error);
+  
+        const toSend = {
+          status: "error",
+          error: error
+        };
+  
+        res.status(500).json(toSend);
+  
+    }
+  });
+
+
+
+
+
+
 
 router.get("/new-user", async (req, res) => {
   try {
