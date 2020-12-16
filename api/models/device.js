fc80404818f01c441ebde2fbc58feb8637ebfd31
Pablo <somosioticos@gmail.com>
2020-12-16 11:45:06 -0300

    device model created... big banners :)

---
+++ api/models/device.js
@@ -0,0 +1,23 @@
+import mongoose from 'mongoose';
+
+const uniqueValidator = require('mongoose-unique-validator');
+
+const Schema = mongoose.Schema;
+
+const deviceSchema = new Schema({
+    userId: { type: String, required: [true] },
+    dId: { type: String, unique: true, required: [true] },
+    name: { type: String, required: [true] },
+    selected: { type: Boolean, required: [true], default: false },
+    templateId: {type: String, required: [true]},
+    templateName: {type: String, required: [true]},
+    createdTime: { type: Number }
+});
+
+// Validator
+deviceSchema.plugin(uniqueValidator, { message: 'Error, device already exists.' });
+
+// Schema to model.
+const Device = mongoose.model('Device', deviceSchema);
+
+export default Device;
