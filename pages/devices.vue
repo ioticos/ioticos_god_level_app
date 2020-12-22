9ea10f84ce5145a182eccee092e56a2a063e6907
Pablo <somosioticos@gmail.com>
2020-12-22 14:55:35 -0300

    create new device view ready

--- pages/devices.vue
+++ pages/devices.vue
@@ -13,6 +13,7 @@
               label="Device Name"
               type="text"
               placeholder="Ex: Home, Office..."
+              v-model="newDevice.name"
             >
             </base-input>
           </div>
@@ -22,6 +23,7 @@
               label="Device Id"
               type="text"
               placeholder="Ex: 7777-8888"
+              v-model="newDevice.dId"
             >
             </base-input>
           </div>
@@ -32,27 +34,17 @@
             </slot>
 
             <el-select
-              value="1"
+              v-model="selectedIndexTemplate"
               placeholder="Select Template"
               class="select-primary"
               style="width:100%"
             >
               <el-option
+                v-for="(template, index) in templates"
+                :key="template._id"
                 class="text-dark"
-                value="Template 1"
-                label="Template 1"
-              ></el-option>
-
-              <el-option
-                class="text-dark"
-                value="Template 2"
-                label="Template 2"
-              ></el-option>
-
-              <el-option
-                class="text-dark"
-                value="Template 3"
-                label="Template 3"
+                :value="index"
+                :label="template.name"
               ></el-option>
             </el-select>
           </div>
@@ -60,7 +52,13 @@
 
         <div class="row pull-right">
           <div class="col-12">
-            <base-button type="primary" class="mb-3" size="lg">Add</base-button>
+            <base-button
+              @click="createNewDevice()"
+              type="primary"
+              class="mb-3"
+              size="lg"
+              >Add</base-button
+            >
           </div>
         </div>
       </card>
@@ -136,7 +134,7 @@
       </card>
     </div>
 
-    <Json :value="$store.state.devices"></Json>
+    <Json :value="templates"></Json>
   </div>
 </template>
 
@@ -153,14 +151,135 @@ export default {
     [Select.name]: Select
   },
   data() {
-    return {};
+    return {
+      templates: [],
+      selectedIndexTemplate: null,
+      newDevice: {
+        name: "",
+        dId: "",
+        templateId: "",
+        templateName: ""
+      }
+    };
   },
   mounted() {
     this.$store.dispatch("getDevices");
+    this.getTemplates();
   },
   methods: {
+    //new device
+    createNewDevice() {
+      if (this.newDevice.name == "") {
+        this.$notify({
+          type: "warning",
+          icon: "tim-icons icon-alert-circle-exc",
+          message: " Device Name is Empty :("
+        });
+        return;
+      }
 
-    
+      if (this.newDevice.dId == "") {
+        this.$notify({
+          type: "warning",
+          icon: "tim-icons icon-alert-circle-exc",
+          message: " Device ID is Empty :("
+        });
+        return;
+      }
+
+      if (this.selectedIndexTemplate == null) {
+        this.$notify({
+          type: "warning",
+          icon: "tim-icons icon-alert-circle-exc",
+          message: " Tempalte must be selected"
+        });
+        return;
+      }
+
+      const axiosHeaders = {
+        headers: {
+          token: this.$store.state.auth.token
+        }
+      };
+
+      //ESCRIBIMOS EL NOMBRE Y EL ID DEL TEMPLATE SELECCIONADO EN EL OBJETO newDevice
+      this.newDevice.templateId = this.templates[
+        this.selectedIndexTemplate
+      ]._id;
+      this.newDevice.templateName = this.templates[
+        this.selectedIndexTemplate
+      ].name;
+
+      const toSend = {
+        newDevice: this.newDevice
+      };
+
+      this.$axios
+        .post("/device", toSend, axiosHeaders)
+        .then(res => {
+
+          if (res.data.status == "success") {
+
+            this.$store.dispatch("getDevices");
+
+            this.newDevice.name = "";
+            this.newDevice.dId = "";
+            this.selectedIndexTemplate = null;
+
+            this.$notify({
+              type: "success",
+              icon: "tim-icons icon-check-2",
+              message: "Success! Device was added"
+            });
+
+            return;
+          }
+        })
+        .catch(e => {
+
+          if (
+            e.response.data.status == "error" &&
+            e.response.data.error.errors.dId.kind == "unique"
+          ) {
+            this.$notify({
+              type: "warning",
+              icon: "tim-icons icon-alert-circle-exc",
+              message:
+                "The device is already registered in the system. Try another device"
+            });
+            return;
+          } else {
+            this.showNotify("danger", "Error");
+            return;
+          }
+        });
+    },
+
+    //Get Templates
+    async getTemplates() {
+      const axiosHeaders = {
+        headers: {
+          token: this.$store.state.auth.token
+        }
+      };
+
+      try {
+        const res = await this.$axios.get("/template", axiosHeaders);
+        console.log(res.data);
+
+        if (res.data.status == "success") {
+          this.templates = res.data.data;
+        }
+      } catch (error) {
+        this.$notify({
+          type: "danger",
+          icon: "tim-icons icon-alert-circle-exc",
+          message: "Error getting templates..."
+        });
+        console.log(error);
+        return;
+      }
+    },
 
     deleteDevice(device) {
       const axiosHeader = {
@@ -175,7 +294,6 @@ export default {
       this.$axios
         .delete("/device", axiosHeader)
         .then(res => {
-
           if (res.data.status == "success") {
             this.$notify({
               type: "success",
@@ -184,7 +302,6 @@ export default {
             });
             this.$store.dispatch("getDevices");
           }
-          
         })
         .catch(e => {
           console.log(e);
