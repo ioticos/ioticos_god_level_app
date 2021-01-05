e68d91a860583b1002c0dcf92606d47d3c3861c7
Pablo <somosioticos@gmail.com>
2021-01-05 17:21:56 -0300

    increment alarm rule counter

--- api/routes/webhooks.js
+++ api/routes/webhooks.js
@@ -6,6 +6,7 @@ const colors = require("colors");
 import Data from "../models/data.js";
 import Device from "../models/device.js";
 import Notification from "../models/notifications.js";
+import AlarmRule from "../models/emqx_alarm_rule.js";
 
 //SAVER WEBHOOK
 router.post("/saver-webhook", async (req, res) => {
@@ -54,14 +55,22 @@ router.post("/alarm-webhook", async (req, res) => {
             req.sendStatus(404);
             return;
           }
+
+          res.sendStatus(200);
+
         
           const incomingAlarm = req.body;
+
+          updateAlarmCounter(incomingAlarm.emqxRuleId);
           
           const lastNotif = await Notification.find({ dId: incomingAlarm.dId, emqxRuleId: incomingAlarm.emqxRuleId }).sort({ time: -1 }).limit(1);
 
           if (lastNotif == 0){
+
             console.log("FIRST TIME ALARM");
             saveNotifToMongo(incomingAlarm);
+            
+
           }else{
 
             const lastNotifToNowMins = ( Date.now() - lastNotif[0].time ) / 1000 / 60;
@@ -71,12 +80,9 @@ router.post("/alarm-webhook", async (req, res) => {
                 saveNotifToMongo(incomingAlarm);
             }
 
-          }
-
+          }     
           
           
-          res.sendStatus(200);
-        
         
     } catch (error) {
         console.log(error);
@@ -88,11 +94,24 @@ router.post("/alarm-webhook", async (req, res) => {
 
 function saveNotifToMongo(incomingAlarm) {
 
-    var newNotif = incomingAlarm;
-    newNotif.time = Date.now();
-    newNotif.readed = false;
-    Notification.create(newNotif);
+    try {
+        var newNotif = incomingAlarm;
+        newNotif.time = Date.now();
+        newNotif.readed = false;
+        Notification.create(newNotif); 
+    } catch (error) {
+        console.log(error)
+    }
 
+}
+
+async function updateAlarmCounter(emqxRuleId) {
+  
+    try {
+       await AlarmRule.update({ emqxRuleId: emqxRuleId }, { $inc: { counter: 1 } });
+    } catch (error) {
+        console.log(error)
+    }
 }
 
 
