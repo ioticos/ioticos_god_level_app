f2c86e673e1fc53ecceb5301c477aa66211ca538
Pablo <somosioticos@gmail.com>
2021-02-08 17:25:21 -0300

    commit

--- api/routes/devices.js
+++ api/routes/devices.js
@@ -277,9 +277,9 @@ async function getSaverRules(userId) {
 //create saver rule
 async function createSaverRule(userId, dId, status) {
 
-
+ 
   try {
-    const url = "http://localhost:8085/api/v4/rules";
+    const url = "http://"+process.env.EMQX_NODE_HOST+":8085/api/v4/rules";
 
     const topic = userId + "/" + dId + "/+/sdata";
 
@@ -330,7 +330,7 @@ async function createSaverRule(userId, dId, status) {
 //update saver rule
 async function updateSaverRuleStatus(emqxRuleId, status) {
   try {
-    const url = "http://localhost:8085/api/v4/rules/" + emqxRuleId;
+    const url = "http://"+process.env.EMQX_NODE_HOST+":8085/api/v4/rules/" + emqxRuleId;
 
     const newRule = {
       enabled: status
@@ -355,7 +355,7 @@ async function deleteSaverRule(dId) {
   try {
     const mongoRule = await SaverRule.findOne({ dId: dId });
 
-    const url = "http://localhost:8085/api/v4/rules/" + mongoRule.emqxRuleId;
+    const url = "http://"+process.env.EMQX_NODE_HOST+":8085/api/v4/rules/" + mongoRule.emqxRuleId;
 
     const emqxRule = await axios.delete(url, auth);
 
@@ -376,7 +376,7 @@ async function deleteAllAlarmRules(userId, dId) {
 
     if (rules.length > 0) {
       asyncForEach(rules, async rule => {
-        const url = "http://localhost:8085/api/v4/rules/" + rule.emqxRuleId;
+        const url = "http://"+process.env.EMQX_NODE_HOST+":8085/api/v4/rules/" + rule.emqxRuleId;
         const res = await axios.delete(url, auth);
       });
 
