814d154f3137026a7345b158ec60e1ed942df1f0
Pablo <somosioticos@gmail.com>
2021-02-09 15:44:42 -0300

    commit

--- api/index.js
+++ api/index.js
@@ -30,13 +30,28 @@ app.use("/api", require("./routes/webhooks.js"));
 app.use("/api", require("./routes/emqxapi.js"));
 app.use("/api", require("./routes/alarms.js"));
 app.use("/api", require("./routes/dataprovider.js"));
-
+ 
 module.exports = app;
 
 //listener
 app.listen(process.env.API_PORT, () => {
   console.log("API server listening on port " + process.env.API_PORT);
 });
+
+
+if (process.env.environment != "dev"){
+
+  const app2 = express();
+
+  app2.listen(3002, function(){
+    console.log("Listening on port 3002 (for redirect to ssl)");
+  });
+  
+  app2.all('*', function(req, res){
+    console.log("NO SSL ACCESS ... REDIRECTING...");
+    return res.redirect("https://" + req.headers["host"] + req.url);
+  });
+}
 
 
 
