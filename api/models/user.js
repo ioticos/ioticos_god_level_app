7e760f69490fbe16efd9a98bfb2005ff484cc9cf
Ricardo Perez <ricardo.pereze.98@gmail.com>
2023-02-15 19:11:27 -0400

    [UPDATE] telegram by Model user

--- api/models/user.js
+++ api/models/user.js
@@ -7,6 +7,7 @@ const userSchema = new Schema({
   name: { type: String, required: [true] },
   email: { type: String, required: [true], unique: true},
   password: {  type: String, required: [true]},
+  telegramID: {  type: Number, required: [false]}
 });
 
 
